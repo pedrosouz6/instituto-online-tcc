ae7b638e4ee5c006716eca39ce7cb601419323ef
Pedro Souza <peedrosouz6@gmail.com>
2022-08-12 10:05:47 -0300

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import GlobalsStyled from '../styles/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalsStyled />
+    </>
+  )
 }
 
 export default MyApp
