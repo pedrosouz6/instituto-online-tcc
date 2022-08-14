ef89e8522b5107e6073aac24c53f0c1cdd0d8d59
Pedro Souza <peedrosouz6@gmail.com>
2022-08-14 20:06:59 -0300

    saving theme preferences in cookies

--- src/contexts/Theme/index.tsx
+++ src/contexts/Theme/index.tsx
@@ -1,5 +1,6 @@
-import { createContext, useState, ReactNode } from "react";
+import { createContext, useState, ReactNode, useEffect } from "react";
 import { ThemeProvider } from 'styled-components';
+import { parseCookies, setCookie } from 'nookies';
 
 import light from '../../../styles/themes/light';
 import dark from '../../../styles/themes/dark';
@@ -20,8 +21,20 @@ export default function ProviderTheme({ children }: ProviderThemeProps) {
     const [ theme, setTheme ] = useState(light);
 
     function toggleTheme() {
+        setCookie(null, 'theme', theme.title === 'light' ? 'light' : 'dark', {
+            maxAge: 30 * 24 * 60 * 60
+        })
+
         setTheme(theme.title === 'light' ? dark : light);
     }
+
+    useEffect(() => {
+        const { ['theme']: isThemeExist } = parseCookies(null);
+
+        if(isThemeExist) {
+            setTheme(isThemeExist === 'light' ? dark : light);
+        }
+    }, []);
 
     return (
         <ContextTheme.Provider value={{
