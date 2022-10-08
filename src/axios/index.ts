31092c89846110c328616b1145d988cd1d53561b
Pedro Souza <peedrosouz6@gmail.com>
2022-10-08 11:59:03 -0300

    adding private pages

--- src/axios/index.ts
+++ src/axios/index.ts
@@ -1,10 +1,18 @@
 import Axios from 'axios';
+import { parseCookies } from 'nookies';
 
 export interface ErrorAxiosType {
     error: boolean,
     message: string
 }
 
+const { ['token_user']: token } = parseCookies(null);
+
 export const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL
 })
+
+axios.interceptors.request.use(function (config: any) {
+    config.headers.Authorization =  token ? `Bearer ${token}` : '';
+    return config;
+});
